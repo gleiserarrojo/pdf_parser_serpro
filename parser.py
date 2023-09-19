8f068999de202d36411f4dcffdb407474420b5ab
Gleiser Arrojo <gleiseralvarez10@gmail.com>
2023-09-18 22:01:47 -0300

    no logic changes, small adjusts

--- parser.py
+++ parser.py
@@ -8,8 +8,9 @@ list = reader.pages[0].extract_text().split('/')
 
 list[0] = list[0].split(',')
 
-# list[0] = list.split('\n')
+# print(list[0])
 
-print(list[0])
+print(list[0][-3].split('\n'))
+
 
 file.close()
