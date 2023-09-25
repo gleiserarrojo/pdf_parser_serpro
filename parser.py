4abc595c0f0e6c5a9e551184dcd6683344c5e10d
Gleiser Arrojo <gleiseralvarez10@gmail.com>
2023-09-25 19:52:24 -0300

    comentarios, candidatos da pag1 ok, remocao espaco

--- parser.py
+++ parser.py
@@ -1,16 +1,23 @@
 import PyPDF2
 
+# abre o pdf para ler
 file = open('Resultado Pratica.PDF', 'rb')
 
+# cria o leitor do pdf
 reader = PyPDF2.PdfReader(file)
 
-list = reader.pages[0].extract_text().split('/') 
+# extrai o texto da primeira página e divide os nomes
+names = reader.pages[0].extract_text().split('/') 
 
-list[0] = list[0].split(',')
-
-# print(list[0])
-
-print(list[0][-3].split('\n'))
-
-
+# fecha o arquivo
 file.close()
+
+# remove todo o texto antes do primeiro nome e salva só as infos do candidato
+names[0] = names[0].split(',')
+aux = names[0][-3].split('\n')
+names[0] = str(aux[-1] + ',' + names[0][-2] + ',' + names[0][-1])
+
+# remove espacos e quebras de linhas (FIX)
+for x in range(len(names)):
+    names[x] = names[x].replace(" ", "").replace("\n", "")
+    print(names[x])
